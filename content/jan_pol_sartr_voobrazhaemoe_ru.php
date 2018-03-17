37d33ac0dcec7e60655bd96f54b8d5981d3edb4a
unknown <prog.sebo@gmail.com>
2018-03-17 11:01:47 +0100

    Change url and add pdf files in content

--- content/jan_pol_sartr_voobrazhaemoe_ru.php
+++ content/jan_pol_sartr_voobrazhaemoe_ru.php
@@ -1,3 +1,3 @@
 ﻿<?php 
-echo '<iframe class="embed" src="http://senses.pro/content/pdf/jan_pol_sartr_voobrazhaemoe_ru.pdf" width="100%" height="500"></iframe>';
+echo '<iframe class="embed" src="http://senses.pro/content/pdf/jean_paul_sartre_voobrazhaemoe_ru.pdf" width="100%" height="500"></iframe>';
 ?>
