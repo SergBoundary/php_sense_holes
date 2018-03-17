de44b2e55323c34baf6a4b4ea264a0502373c225
unknown <prog.sebo@gmail.com>
2018-03-17 10:30:56 +0100

    Change url for pdf files in content

--- content/martin_heidegger_osnovnye_problemy_phenomenologii_ru.php
+++ content/martin_heidegger_osnovnye_problemy_phenomenologii_ru.php
@@ -1,3 +1,3 @@
 ﻿<?php 
-echo '<iframe class="embed" src="http://sebo.com/content/pdf/martin_heidegger_osnovnye_problemy_phenomenologii_ru.pdf" width="100%" height="500"></iframe>';
+echo '<iframe class="embed" src="http://senses.pro/content/pdf/martin_heidegger_osnovnye_problemy_phenomenologii_ru.pdf" width="100%" height="500"></iframe>';
 ?>
--- content/martin_heidegger_parmenid_ru.php
+++ content/martin_heidegger_parmenid_ru.php
@@ -1,3 +1,3 @@
 ﻿<?php 
-echo '<iframe class="embed" src="http://sebo.com/content/pdf/martin_heidegger_parmenid_ru.pdf" width="100%" height="500"></iframe>';
+echo '<iframe class="embed" src="http://senses.pro/content/pdf/martin_heidegger_parmenid_ru.pdf" width="100%" height="500"></iframe>';
 ?>
